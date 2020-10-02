8a82d71b64a98ff11aa2f33ae1ac634a8c88e358
nicole <nicole.aretz@t-online.de>
2020-10-02 23:18:19 +0200

    just testing if I can make changes

--- Assets/Scripts/GithubTestScript.cs
+++ Assets/Scripts/GithubTestScript.cs
@@ -2,6 +2,7 @@
 
 public class GithubTestScript : MonoBehaviour {
     // Start is called before the first frame update
+    // test if I can push to the repository
     void Start() { }
 
     // Update is called once per frame

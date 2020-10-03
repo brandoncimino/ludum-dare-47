7fead6b2d5459a6de51f68eda6a078e1a4d4fad2
GuyWithPasta <guywithpasta@gmail.com>
2020-10-03 16:28:54 -0500

    Added foundation of Station functionality

---
+++ Assets/Scripts/BehaveStationStats.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using DefaultNamespace;
+
+using UnityEngine;
+
+public class BehaveStationStats : ActivityStation
+{
+    public enum BehaveStationStates {
+        Occupied,
+        Abandoned
+    }
+
+    public BehaveStationStates currentState = BehaveStationStates.Abandoned;
+
+    
+
+    public void Abandon() {
+        currentState = BehaveStationStates.Abandoned;
+    }
+}
---
+++ Assets/Scripts/MisbehaveStationStats.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using DefaultNamespace;
+
+using UnityEngine;
+
+public class MisbehaveStationStats : ActivityStation
+{
+    public enum MisbehaveStationStates {
+        Fixed,
+        Broken
+    }
+    
+    public static GameObject         BehaveTwin;
+    private       BehaveStationStats BehaveTwinStats = BehaveTwin.GetComponent<BehaveStationStats>();
+
+    public BehaveStationStats GetTwinStats() {
+        return BehaveTwinStats;
+    }
+    
+    public MisbehaveStationStates currentState = MisbehaveStationStates.Fixed;
+}

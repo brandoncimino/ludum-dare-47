911df64f3b5bdbc81f75c695c65bf3f8d9d2ed71
nicole <nicole.aretz@t-online.de>
2020-10-03 23:52:03 +0200

    Nicole: astronaut moves along space station surface

--- Assets/Scripts/AstronautVisuals.cs
+++ Assets/Scripts/AstronautVisuals.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Random = System.Random;
@@ -9,6 +10,17 @@ public class AstronautVisuals : MonoBehaviour
     public GameObject myStation;
     Random rando = new Random();
 
+    // degree of speed, as an element between 0 and 360 degree, for the moment as int
+    public float speedAngle = 20;
+    
+    // angle that describes where I am on the circle that is my space station
+    public float positionAngle = 0;
+    
+    // radius of the space station
+    public double radiusSpacestation = 14.1033;
+    
+    // target location
+    public float   targetAngle = 90;
     
     // Start is called before the first frame update
     void Start()
@@ -19,10 +31,64 @@ public class AstronautVisuals : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.localEulerAngles = new Vector3(0,0,Vector3.Angle(transform.position, transform.parent.position));
-        //unfinished testing if statements
         if (Input.GetKeyDown(KeyCode.A)) {
-            //transform.position;
+            MoveTowardTarget();
         }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            MoveClockwise();
+        }
+        
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            MoveCounterClockwise();
+        }
+
     }
+
+    void MoveTowardTarget() {
+        if (Math.Abs(targetAngle - positionAngle) < 180) {
+            positionAngle = (positionAngle + Math.Sign(targetAngle-positionAngle) * speedAngle) % 360;
+        }
+        else {
+            positionAngle = (positionAngle - Math.Sign(targetAngle-positionAngle) * speedAngle + 360) % 360;
+        }
+            
+        var angle = Math.PI * (positionAngle) / 180;
+        transform.localPosition = new Vector3(
+            (float) (radiusSpacestation * Math.Cos(angle)),
+            0,
+            (float) (radiusSpacestation * Math.Sin(angle))
+        );
+            
+        // transform.Rotate(new Vector3(1, 0, 1), speedAngle);
+        transform.localEulerAngles = new Vector3(90, 0, positionAngle+90);
+    }
+    
+    
+    void MoveClockwise() {
+        positionAngle = (positionAngle - speedAngle + 360) % 360;
+        var angle = Math.PI * (positionAngle) / 180;
+        transform.localPosition = new Vector3(
+            (float) (radiusSpacestation * Math.Cos(angle)),
+            0,
+            (float) (radiusSpacestation * Math.Sin(angle))
+        );
+            
+        // transform.Rotate(new Vector3(1, 0, 1), speedAngle);
+        transform.localEulerAngles = new Vector3(90, 0, positionAngle+90);
+    }
+    
+    void MoveCounterClockwise() {
+        positionAngle = (positionAngle + speedAngle) % 360;
+        var angle = Math.PI * (positionAngle) / 180;
+        transform.localPosition = new Vector3(
+            (float) (radiusSpacestation * Math.Cos(angle)),
+            0,
+            (float) (radiusSpacestation * Math.Sin(angle))
+        );
+            
+        // transform.Rotate(new Vector3(1, 0, 1), speedAngle);
+        transform.localEulerAngles = new Vector3(90, 0, positionAngle+90);
+    }
+    
 }

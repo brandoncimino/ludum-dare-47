c7fe4e1cd5dd473ea24a5e12548f898023f7f685
brandoncimino <brandon.cimino@gmail.com>
2020-10-03 18:04:51 -0500

    Added stuff to render thought bubbles

---
+++ Assets/Scripts/Thinker.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace {
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class Thinker : MonoBehaviour {
+        public Thought CurrentThought;
+    }
+}

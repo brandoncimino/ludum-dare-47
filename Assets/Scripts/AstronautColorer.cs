ef0a95a542144df6f03b160284e837710d077a1f
Michael <michael.b.rawls@gmail.com>
2020-10-03 13:57:50 -0500

    Created SpaceStation4 with back wall, astronaut with random color

---
+++ Assets/Scripts/AstronautColorer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+public class AstronautColorer : MonoBehaviour
+{
+    public SpriteRenderer mySpriteRenderer;
+    Random rando = new Random();
+    // Start is called before the first frame update
+    void Start()
+    {
+        mySpriteRenderer.color = new Color(rando.Next(0,255),rando.Next(0,255),rando.Next(0,255));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

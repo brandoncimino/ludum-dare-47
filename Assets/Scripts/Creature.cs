54a46ed1397d4e0100790c9491ad599ddaa5d2bd
brandoncimino <brandon.cimino@gmail.com>
2020-10-04 12:02:14 -0500

    Turned creatures to face where they're going

--- Assets/Scripts/Creature.cs
+++ Assets/Scripts/Creature.cs
@@ -1,52 +1,57 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 
 using DefaultNamespace;
 
 using Packages.BrandonUtils.Editor;
 
 using UnityEngine;
+
 using Random = System.Random;
 
-public class Creature : MonoBehaviour
-{
+public class Creature : MonoBehaviour {
     public SpriteRenderer mySpriteRenderer;
-    public GameObject myStation;
-    Random rando = new Random();
+    public GameObject     myStation;
+    Random                rando = new Random();
 
     // movement and positional information
+
+    #region Movement & Position
+
     public float positionAngle = 0;
     public float speedAngle    = 1;
     public int   layer         = 0;
-    
+
+    #endregion
+
     // target location
     public float targetAngle = 90;
-    
-    // informationo about the space station
-    // TODO: call SpaceStation class instad
+
+    // information about the space station
+    // TODO: call SpaceStation class instead
     public SpaceStation home;
-    
-    
+
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         // randomly assign a color
-        mySpriteRenderer.color = new Color(rando.Next(0,255)/255f,rando.Next(0,255)/255f,rando.Next(0,255)/255f);
-        
+        mySpriteRenderer.color = new Color(
+            rando.Next(0, 255) / 255f,
+            rando.Next(0, 255) / 255f,
+            rando.Next(0, 255) / 255f
+        );
+
         // resize to the correct size (relative to parent space station)
         transform.localScale = new Vector3(5.243802f, 5.243802f, 5.243802f);
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         // the creature moves toward its target location
         if (Distance2TargetAsAngle() > speedAngle) {
             MoveTowardTarget();
         }
-
     }
+
     public void ChangeLayer(int newLayer) {
         layer = newLayer;
     }
@@ -54,28 +59,33 @@ public class Creature : MonoBehaviour
     public void GiveHome(SpaceStation newHome) {
         home = newHome;
     }
+
     private void MoveTowardTarget() {
         // TODO: change direction into which the astronaut looks
-        
+
         if (Math.Abs(targetAngle - positionAngle) < 180) {
-            positionAngle = (positionAngle + Math.Sign(targetAngle-positionAngle) * speedAngle * Time.deltaTime) % 360;
+            positionAngle = (positionAngle + Math.Sign(targetAngle - positionAngle) * speedAngle * Time.deltaTime) %
+                            360;
+            mySpriteRenderer.flipX = true;
         }
         else {
-            positionAngle = (positionAngle - Math.Sign(targetAngle-positionAngle) * speedAngle * Time.deltaTime + 360) % 360;
+            positionAngle =
+                (positionAngle - Math.Sign(targetAngle - positionAngle) * speedAngle * Time.deltaTime + 360) % 360;
+            mySpriteRenderer.flipX = false;
         }
-            
+
         var       angle = Math.PI * (positionAngle) / 180;
         Transform transform1;
         (transform1 = transform).localPosition = new Vector3(
             (float) (home.radius * Math.Cos(angle)),
-            -(home.depth/2) + layer * home.depth / home.noAstronauts,
+            -(home.depth / 2) + layer * home.depth / home.noAstronauts,
             (float) (home.radius * Math.Sin(angle))
         );
-            
+
         // transform.Rotate(new Vector3(1, 0, 1), speedAngle);
-        transform1.localEulerAngles = new Vector3(90, 0, positionAngle+90);
+        transform1.localEulerAngles = new Vector3(90, 0, positionAngle + 90);
     }
-    
+
     [EditorInvocationButton]
     public void MoveClockwise() {
         positionAngle = (positionAngle - speedAngle + 360) % 360;
@@ -86,25 +96,25 @@ public class Creature : MonoBehaviour
             0,
             (float) (home.radius * Math.Sin(angle))
         );
-            
+
         // transform.Rotate(new Vector3(1, 0, 1), speedAngle);
-        transform1.localEulerAngles = new Vector3(90, 0, positionAngle+90);
+        transform1.localEulerAngles = new Vector3(90, 0, positionAngle + 90);
     }
-    
+
     [EditorInvocationButton]
     public void MoveCounterClockwise() {
         positionAngle = (positionAngle + speedAngle) % 360;
         var       angle = Math.PI * (positionAngle) / 180;
         Transform transform1;
-        
+
         (transform1 = transform).localPosition = new Vector3(
             (float) (home.radius * Math.Cos(angle)),
             0,
             (float) (home.radius * Math.Sin(angle))
         );
-            
+
         // transform.Rotate(new Vector3(1, 0, 1), speedAngle);
-        transform1.localEulerAngles = new Vector3(90, 0, positionAngle+90);
+        transform1.localEulerAngles = new Vector3(90, 0, positionAngle + 90);
     }
 
     public void SetTarget(float newAngle) {
@@ -115,26 +125,29 @@ public class Creature : MonoBehaviour
         // location is in the global coordinate system
         // TODO: once we have a space station class that knows about its rotation, this projection needs to be corrected
         // right now this function assumes that the station is rotated so that its normal is the z-direction
-        
+
         // get difference to center of the station
         var diff = location - transform.parent.position;
-        
+
         // project into 2-dim space
         diff.z = 0;
-        
+
         // project onto unit circle
         if (diff.magnitude < 1e-14) {
             // make sure we don't divide by zero
-            throw new ArithmeticException("In SetTarget: Projection onto spaceship-plane ended too close to the center");
+            throw new ArithmeticException(
+                "In SetTarget: Projection onto spaceship-plane ended too close to the center"
+            );
         }
+
         diff /= diff.magnitude;
-        
+
         // get angle as radian
         var angle = Math.Acos(diff.x);
         if (diff.y < 0) {
             angle += Math.PI;
         }
-        
+
         // transform to degree
         targetAngle = (float) ((float) angle * 180 / Math.PI);
     }
@@ -146,14 +159,17 @@ public class Creature : MonoBehaviour
     public float Distance2Target() {
         return home.radius * ((float) Math.PI) * Distance2TargetAsAngle() / 180f;
     }
-    
+
     private void OnMouseDown() {
         if (Input.GetMouseButtonDown(0)) {
-            
             // TODO: change AI behaviour
 
             // randomly assign a new color
-            mySpriteRenderer.color = new Color(rando.Next(0, 255)/255f, rando.Next(0, 255)/255f, rando.Next(0,255)/255f);
+            mySpriteRenderer.color = new Color(
+                rando.Next(0, 255) / 255f,
+                rando.Next(0, 255) / 255f,
+                rando.Next(0, 255) / 255f
+            );
         }
     }
 }
--- Assets/Scripts/SpaceStation.cs
+++ Assets/Scripts/SpaceStation.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 
 using Packages.BrandonUtils.Editor;
@@ -7,17 +6,16 @@ using UnityEngine;
 
 namespace DefaultNamespace {
     public class SpaceStation : MonoBehaviour {
-
         // geoometric information about the space station
         // space station floor is assumed as a cylinder for simplicity
         public float radius = 14.1033f;
         public float depth  = 6f;
-        
+
         // information about the astronauts living on the space station
         public GameObject     AstronautPrefab;
         public List<Creature> Astronauts;
         public int            noAstronauts = 5;
-        
+
         // information about the station's spin
         private       float idealSpeed          = 10f;
         public        float Speed               = 10f; // degrees per time step
@@ -26,7 +24,7 @@ namespace DefaultNamespace {
         private const float xRotationMod        = 0.3f;
         private       float tumbleDegree        = 0;
         private       bool  tumble2Player       = true;
-        
+
         void Start() {
             // spawn astronauts
             for (int i = 0; i < noAstronauts; i++) {
@@ -36,30 +34,28 @@ namespace DefaultNamespace {
                 newAstronaut.GiveHome(this);
                 Astronauts.Add(newAstronaut);
             }
-            
         }
 
         void Update() {
-            
             // compute speed (explicit Euler - exact if the acceleration is piecewise constant)
-            Speed       += Time.deltaTime * AccelerationMod * ActiveAccelerations;
-            
+            Speed += Time.deltaTime * AccelerationMod * ActiveAccelerations;
+
             // rotate
             transform.Rotate(0, Speed * Time.deltaTime, 0);
 
             if (tumble2Player) {
                 tumbleDegree += Speed * Time.deltaTime;
             }
-            
+
             // transform.Rotate(Speed * (float) Math.Sin(Time.time) * xRotationMod, 0,     0, Space.World);
             // TODO: tumbling, needs some fine-tuning
         }
-        
+
         [EditorInvocationButton]
         public void Accelerate() {
             ActiveAccelerations++;
         }
-        
+
         [EditorInvocationButton]
         public void Decelerate() {
             ActiveAccelerations--;

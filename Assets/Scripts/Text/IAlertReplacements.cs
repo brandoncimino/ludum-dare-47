11c31a2ecd54bf4a6612dd6cdff0123dcaddd344
brandoncimino <brandon.cimino@gmail.com>
2020-10-15 10:11:39 -0500

    More comments

--- Assets/Scripts/Text/IAlertReplacements.cs
+++ Assets/Scripts/Text/IAlertReplacements.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.Generic;
 
 namespace DefaultNamespace.Text {
+    /// <summary>
+    /// This interface generates "replacements" for use when formatting <see cref="StationLog.Alerts"/> via methods like <see cref="StationLog.FormatAlert(string,System.Collections.Generic.Dictionary{string,string})"/>.
+    /// </summary>
     public interface IAlertReplacements {
         /// <summary>
         /// Returns a <see cref="Dictionary{TKey,TValue}"/> to be used as the replacements in <see cref="StationLog.FormatAlert(string,System.Collections.Generic.Dictionary{string,string})"/>.

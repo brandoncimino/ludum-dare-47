2b75bfe37aa6b597ed79208579137fd5432c1ef1
brandoncimino <brandon.cimino@gmail.com>
2020-10-15 10:07:09 -0500

    Added IAlertReplacements.cs

---
+++ Assets/Scripts/Text/IAlertReplacements.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace DefaultNamespace.Text {
+    public interface IAlertReplacements {
+        /// <summary>
+        /// Returns a <see cref="Dictionary{TKey,TValue}"/> to be used as the replacements in <see cref="StationLog.FormatAlert(string,System.Collections.Generic.Dictionary{string,string})"/>.
+        /// </summary>
+        /// <returns></returns>
+        Dictionary<string, string> GetAlertReplacements();
+    }
+}

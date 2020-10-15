e7b488ce8f3ea819900b0b457a2926e5e95c6e3b
brandoncimino <brandon.cimino@gmail.com>
2020-10-15 13:58:34 -0500

    - Added StationLogger.SoupOfTheDay - Rendered a default message if an alert isn't properly found

--- Assets/Scripts/Text/StationLogger.cs
+++ Assets/Scripts/Text/StationLogger.cs
@@ -1,5 +1,8 @@
-﻿using Newtonsoft.Json;
+﻿using System.Collections.Generic;
 
+using Newtonsoft.Json;
+
+using Packages.BrandonUtils.Runtime;
 using Packages.BrandonUtils.Runtime.Collections;
 
 using TMPro;
@@ -33,9 +36,26 @@ namespace DefaultNamespace.Text {
         }
 
         public static void Log(StationAlertType alertType, params IAlertReplacements[] replacementSources) {
-            var alert     = Single.StationLog.Alerts[alertType].Random();
-            var formatted = StationLog.FormatAlert(alert, replacementSources);
-            Single.StationLogMesh.text += $"\n{formatted}";
+            string message;
+            try {
+                var alert = Single.StationLog.Alerts[alertType].Random();
+                message = StationLog.FormatAlert(alert, replacementSources);
+            }
+            catch (KeyNotFoundException e) {
+                var error = $"Missing {alertType.GetType().Name}".Colorize(Color.red);
+                message = $"{error}: {alertType}";
+            }
+
+            Single.StationLogMesh.text += $"\n{message}";
         }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <remarks>
+        /// As of 10/15/2020, this is NOT actually based on the day/night cycle.
+        /// </remarks>
+        /// <returns></returns>
+        public static Soup SoupOfTheDay => Single.SoupDatabase.Soups.Random();
     }
 }

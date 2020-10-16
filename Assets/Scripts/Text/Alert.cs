4569aef10db9a5c9965cbc40bbc58aa1c59c5e7e
brandoncimino <brandon.cimino@gmail.com>
2020-10-16 15:31:51 -0500

    Updated scene with fancy alerts! Wooh!

---
+++ Assets/Scripts/Text/Alert.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using UnityEngine;
+
+namespace DefaultNamespace.Text {
+    public class Alert {
+        /// <summary>
+        /// The "severity" of the alert.
+        /// </summary>
+        /// <remarks>
+        /// Roughly corresponds to "logging levels", i.e. <see cref="Debug.Log(object)"/>, <see cref="Debug.LogWarning(object)"/>, etc.
+        /// </remarks>
+        public enum Severity {
+            Info,
+            Warning,
+            Emergency
+        }
+
+        public StationAlertType AlertType;
+
+        /// <summary>
+        /// The formatted message returned by <see cref="StationLog.FormatAlert(string,System.Collections.Generic.Dictionary{string,string})"/>.
+        /// </summary>
+        public string AlertMessage;
+
+        public DateTime TimeStamp;
+
+        private const string TimeStampFormat = "m:ss";
+    }
+}
--- Assets/Scripts/Text/AlertRenderer.cs
+++ Assets/Scripts/Text/AlertRenderer.cs
@@ -1,5 +1,34 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+
+using Packages.BrandonUtils.Runtime;
+
+using TMPro;
+
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace DefaultNamespace.Text {
-    public class AlertRenderer : MonoBehaviour { }
+    public class AlertRenderer : MonoBehaviour {
+        public TMP_Text Mesh;
+        public Image    Background;
+        public Alert    Alert;
+
+        #region Severity
+
+        [Header("Severity Colors")]
+        public List<Color> SeverityColors;
+
+        #endregion
+
+        [EditorInvocationButton]
+        private void SetReferencesToDefault() {
+            Mesh       = GetComponentInChildren<TMP_Text>();
+            Background = GetComponentInChildren<Image>();
+        }
+
+        private void UpdateRenderer() {
+            Mesh.text = Alert.AlertMessage;
+            //TODO: set the background color based on severity
+        }
+    }
 }
